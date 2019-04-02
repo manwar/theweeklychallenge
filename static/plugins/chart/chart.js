27c1c558244d60549dbef39b629af48285cbcf21
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2019-04-02 14:29:00 +0100

    - Updated chart with regard to the challenge 002.

--- static/plugins/chart/chart.js
+++ static/plugins/chart/chart.js
@@ -35,3 +35,20 @@ $("#pwc_current").click(function() {
 
     $('#pwc-spinner').hide();
 });
+
+$("#pwc_challenge_001").click(function() {
+    $('#pwc-spinner').show();
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'pwc-challenge-001.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            var response = JSON.parse(xobj.responseText);
+            $('#pwc_challenge_001_stats').highcharts(response);
+        }
+    };
+    xobj.send(null);
+
+    $('#pwc-spinner').hide();
+});
