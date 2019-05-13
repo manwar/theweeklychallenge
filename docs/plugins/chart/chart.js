cbd2007f98534a289d5c91d35c4c5aa276a3d650
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2019-05-13 12:04:23 +0100

    - Added solution by Neil Bowers.

--- docs/plugins/chart/chart.js
+++ docs/plugins/chart/chart.js
@@ -107,6 +107,23 @@ $("#pwc_current").click(function() {
     $('#pwc-challenges-spinner').hide();
 });
 
+$("#pwc_challenge_007").click(function() {
+    $('#pwc-challenges-spinner').show();
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'pwc-challenge-007.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            var response = JSON.parse(xobj.responseText);
+            $('#pwc_challenge_007_stats').highcharts(response);
+        }
+    };
+    xobj.send(null);
+
+    $('#pwc-challenges-spinner').hide();
+});
+
 $("#pwc_challenge_006").click(function() {
     $('#pwc-challenges-spinner').show();
 
