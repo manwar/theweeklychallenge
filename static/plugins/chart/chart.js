4f7c0e26839a2390c047e830a82f742ab3c7a5ff
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2019-05-20 12:11:24 +0100

    - Added solution by Scimon Proctor. Also updated historical stats for Jaldhar H. Vyas.

--- static/plugins/chart/chart.js
+++ static/plugins/chart/chart.js
@@ -107,6 +107,23 @@ $("#pwc_current").click(function() {
     $('#pwc-challenges-spinner').hide();
 });
 
+$("#pwc_challenge_008").click(function() {
+    $('#pwc-challenges-spinner').show();
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'pwc-challenge-008.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            var response = JSON.parse(xobj.responseText);
+            $('#pwc_challenge_008_stats').highcharts(response);
+        }
+    };
+    xobj.send(null);
+
+    $('#pwc-challenges-spinner').hide();
+});
+
 $("#pwc_challenge_007").click(function() {
     $('#pwc-challenges-spinner').show();
 
