3a03653b00ee047f859e0da532d7951a4746a678
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2019-10-14 11:36:46 +0100

    - Updated Task #1 wording slightly.

--- content/blog/perl-weekly-challenge-030.md
+++ content/blog/perl-weekly-challenge-030.md
@@ -109,7 +109,7 @@ Also please share your creative solutions in other languages.
 ***
 ***
 
-> Write a script to print all possible series of 3 numbers, where in each series at least one of the number is even and sum of the three numbers is always 12. For example, **3,4,5**.
+> Write a script to print all possible series of 3 positive numbers, where in each series at least one of the number is even and sum of the three numbers is always 12. For example, **3,4,5**.
 
 ***
 ***
--- docs/plugins/chart/chart.js
+++ docs/plugins/chart/chart.js
@@ -141,6 +141,23 @@ $("#pwc_current").click(function() {
     $('#pwc-challenges-spinner').hide();
 });
 
+$("#pwc_challenge_029").click(function() {
+    $('#pwc-challenges-spinner').show();
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'pwc-challenge-029.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            var response = JSON.parse(xobj.responseText);
+            $('#pwc_challenge_029_stats').highcharts(response);
+        }
+    };
+    xobj.send(null);
+
+    $('#pwc-challenges-spinner').hide();
+});
+
 $("#pwc_challenge_028").click(function() {
     $('#pwc-challenges-spinner').show();
 
