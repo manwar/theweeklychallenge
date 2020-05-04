dd6f9a7ad67cc9d1cd0de03b6f795ea3e108bc74
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2020-05-04 04:30:15 +0100

    - Added js handler for Challenge 058.

--- docs/plugins/chart/chart.js
+++ docs/plugins/chart/chart.js
@@ -158,6 +158,23 @@ $("#pwc_current").click(function() {
     $('#pwc-challenges-spinner').hide();
 });
 
+$("#pwc_challenge_058").click(function() {
+    $('#pwc-challenges-spinner').show();
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'pwc-challenge-058.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            var response = JSON.parse(xobj.responseText);
+            $('#pwc_challenge_058_stats').highcharts(response);
+        }
+    };
+    xobj.send(null);
+
+    $('#pwc-challenges-spinner').hide();
+});
+
 $("#pwc_challenge_057").click(function() {
     $('#pwc-challenges-spinner').show();
 
