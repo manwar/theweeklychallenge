cd492f11b5056328411013bd35cd8ada24ad029b
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2020-05-18 02:19:58 +0100

    - Updated chart.js.

--- static/plugins/chart/chart.js
+++ static/plugins/chart/chart.js
@@ -158,6 +158,23 @@ $("#pwc_current").click(function() {
     $('#pwc-challenges-spinner').hide();
 });
 
+$("#pwc_challenge_060").click(function() {
+    $('#pwc-challenges-spinner').show();
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'pwc-challenge-060.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            var response = JSON.parse(xobj.responseText);
+            $('#pwc_challenge_060_stats').highcharts(response);
+        }
+    };
+    xobj.send(null);
+
+    $('#pwc-challenges-spinner').hide();
+});
+
 $("#pwc_challenge_059").click(function() {
     $('#pwc-challenges-spinner').show();
 
