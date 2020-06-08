a3e683f360f258e08e5c8fcda7d3d31ddc0cc0dd
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2020-06-08 12:46:12 +0100

    - Added solutions by Luca Ferrari.

--- docs/plugins/chart/chart.js
+++ docs/plugins/chart/chart.js
@@ -158,6 +158,23 @@ $("#pwc_current").click(function() {
     $('#pwc-challenges-spinner').hide();
 });
 
+$("#pwc_challenge_063").click(function() {
+    $('#pwc-challenges-spinner').show();
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'pwc-challenge-063.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            var response = JSON.parse(xobj.responseText);
+            $('#pwc_challenge_063_stats').highcharts(response);
+        }
+    };
+    xobj.send(null);
+
+    $('#pwc-challenges-spinner').hide();
+});
+
 $("#pwc_challenge_062").click(function() {
     $('#pwc-challenges-spinner').show();
 
